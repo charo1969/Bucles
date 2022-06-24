fb9f93ddf97d1f714d52f9a325a8394cb0676cb7
charo <charo@charo>
2022-06-24 19:38:55 +0200

    Ejercicio del1 al 3

---
+++ src/Ejercicio2.java
@@ -0,0 +1,33 @@
+
+	/**
+	 * 2. álgoritmo que imprima la tabla de multiplicar correspondiente a un número comprendido
+	 * 		entre uno y diez, introducido por el usuario, usando un esquema  while.
+	 */
+		import java.util.Scanner;
+
+		public class Ejercicio2 {
+
+			public static void main(String[] args) {
+				Scanner teclado = new Scanner(System.in);
+
+				System.out.println("tablas de multiplicar");
+				
+				int num=0;
+				while(num<1 || num>10) {
+					System.out.println("Introduce un número del 1 al 10");
+					 num = teclado.nextInt();
+					
+				}
+				
+				
+
+				int i = 1;
+				
+				while (  i <= 10) {
+					System.out.printf("%d x %d = %d\n", num, i, num * i);
+					i++;
+				}
+
+	}
+
+}
